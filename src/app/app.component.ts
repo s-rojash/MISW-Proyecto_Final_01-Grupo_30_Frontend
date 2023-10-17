754e55eb28e1017ecc185fef9ea8f34823dc294b
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2023-10-17 00:18:27 -0500

    Se agregan controles para la pagina de Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontabc-front';
+
+  showHead: boolean = false;
+
+  ngOnInit() {
+  }
+
+  constructor(private router: Router) {
+  // on route change to '/login', set the variable showHead to false
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        console.log(event['url'].toString());
+        if (event['url'] == '/login' || event['url'] == '/') {
+          this.showHead = false;
+          console.log("entro false");
+        } else {
+          // console.log("NU")
+          this.showHead = true;
+          console.log("entro true");
+        }
+      }
+    });
+  }
 }
---
+++ src/app/login/login-page/login-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+})
+export class LoginPageComponent implements OnInit  {
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  goToApp(){}
+
+  goToSignUp(){}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    });
+  }
+}
