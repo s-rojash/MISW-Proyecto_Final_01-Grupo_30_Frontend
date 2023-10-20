6164a1d44c28d9721be8485dbfd3d60f78d0cfe3
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2023-10-20 09:17:19 -0500

    Ultimos cambios front end registro de empresas y candidatos

--- src/app/signup/signup-applicant/signup-applicant.component.ts
+++ src/app/signup/signup-applicant/signup-applicant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-applicant',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupApplicantComponent implements OnInit {
 
-  constructor() { }
+  applicantRForm!: FormGroup;
+  selectedtypeLogin: string = "";
+  valueEmail = '';
+  valueNames = '';
+  valuelastNames = '';
+  valuelastNI = '';
+  valuePhone = '';
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  selectChangeHandler (event: any) {
+    this.selectedtypeLogin = event.target.value;
+  }
+
+  registerApplicant(){}
 
   ngOnInit() {
+    this.selectedtypeLogin = "0";
+    this.applicantRForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      names: ["", [Validators.required]],
+      lastnames: ["", [Validators.required]],
+      ni: ["", [Validators.required]],
+      phone: ["", [Validators.required]],
+      typeLogin: this.formBuilder.group({
+        id: [this.selectedtypeLogin]
+      })
+    });
   }
 
 }
