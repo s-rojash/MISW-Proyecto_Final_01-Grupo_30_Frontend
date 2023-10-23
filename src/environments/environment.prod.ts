8a58bbd2058748dfc8a84f204ecb76247c6e9ec1
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2023-10-22 23:11:50 -0500

    Se agrega variable faltante a enviroments.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 const baseUrl = 'https://ms-candidatos.azurewebsites.net';
 const baseUrlCom = 'https://ms-empresas.azurewebsites.net';
+const baseUrlProyectos = 'https://ms-proyectos.azurewebsites.net';
 const idMuseum = "";
 
 export const environment = {
   production: true,
   baseUrl,
   baseUrlCom,
+  baseUrlProyectos,
   idMuseum
 };
