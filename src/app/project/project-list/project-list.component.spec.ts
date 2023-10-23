cfeb2c72fb4302205e507b748d9562209de59ad4
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2023-10-22 23:05:21 -0500

    Se corrigen pruebas con jasmine y karma angular 16

--- src/app/project/project-list/project-list.component.spec.ts
+++ src/app/project/project-list/project-list.component.spec.ts
@@ -2,8 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { MatDialogModule} from '@angular/material/dialog';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectListComponent } from './project-list.component';
+import { ToastrModule } from 'ngx-toastr';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 describe('ProjectListComponent', () => {
   let component: ProjectListComponent;
@@ -11,6 +26,20 @@ describe('ProjectListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[MatDialogModule, HttpClientModule, MatCardModule, MatFormFieldModule, MatButtonModule, MatIconModule,
+        MatInputModule, ReactiveFormsModule, BrowserAnimationsModule,
+        TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+        }),
+        ToastrModule.forRoot({
+          timeOut: 10000,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true,
+      }),],
       declarations: [ ProjectListComponent ]
     })
     .compileComponents();
--- src/app/project/project.service.spec.ts
+++ src/app/project/project.service.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ProjectService } from './project.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('Service: Project', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports:[HttpClientModule],
       providers: [ProjectService]
     });
   });
