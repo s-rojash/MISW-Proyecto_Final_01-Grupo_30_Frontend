8a8aed728c68a2a0b5f22d34555f976acab15e6f
Brayan Hernandez <bs.hernandezb1@uniandes.edu.co>
2023-11-09 13:45:18 -0500

    Se suben las pruebas de lista de categorias

--- src/app/banco-preguntas/categorias-list/categorias-list.component.spec.ts
+++ src/app/banco-preguntas/categorias-list/categorias-list.component.spec.ts
@@ -13,6 +13,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { CategoriasListComponent } from './categorias-list.component';
+import { BancoPreguntasService } from '../banco-preguntas.service';
+import { of } from 'rxjs';
+import { Categoria } from '../categoria';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
@@ -21,6 +24,7 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
 describe('LoginPageComponent', () => {
   let component: CategoriasListComponent;
   let fixture: ComponentFixture<CategoriasListComponent>;
+  let bancoPreguntasService: BancoPreguntasService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -38,7 +42,8 @@ describe('LoginPageComponent', () => {
         positionClass: 'toast-bottom-right',
         preventDuplicates: true,
       }),],
-      declarations: [ CategoriasListComponent ]
+      declarations: [ CategoriasListComponent ],
+      providers: [BancoPreguntasService]
     })
     .compileComponents();
   }));
@@ -46,10 +51,53 @@ describe('LoginPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CategoriasListComponent);
     component = fixture.componentInstance;
+    bancoPreguntasService = TestBed.inject(BancoPreguntasService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should call getListaCategorias getCategorias and return response success", () => {
+    let response: Categoria[] = [{ id: 1, nombre: 'sojash' }];
+
+    spyOn(bancoPreguntasService, 'getCategorias').and.returnValue(of(response));
+    component.idCategoriaSeleccionada = 1;
+
+    component.getListaCategorias();
+    fixture.detectChanges();
+    expect(component.listaCategorias).toEqual(response);
+  });
+
+  it('should set idCategoriaSeleccionada and emit categoriaSeleccionada event', () => {
+    // Arrange
+    const id = 1;
+    const nombre = 'TestCategory';
+
+    // Create a spy for the emit method of categoriaSeleccionada
+    spyOn(component.categoriaSeleccionada, 'emit');
+    component.seleccionarCategoria(id, nombre);
+
+    expect(component.idCategoriaSeleccionada).toBe(id);
+    expect(component.categoriaSeleccionada.emit).toHaveBeenCalledWith({
+      idCategoriaSeleccionada: id,
+      nombreCategoriaSeleccionada: nombre,
+    });
+  });
+
+  it('should call getListaCategorias getCategorias and return response success html', () => {
+    let response: Categoria[] = [{ id: 1, nombre: 'sojash' }, { id: 2, nombre: 'java' }, { id: 3, nombre: 'php' }];
+
+    spyOn(bancoPreguntasService, 'getCategorias').and.returnValue(of(response));
+    component.idCategoriaSeleccionada = 1;
+
+    component.getListaCategorias();
+    fixture.detectChanges();
+
+    const elements= fixture.nativeElement.querySelectorAll('li');
+    expect(elements[0].textContent).toEqual(' ' + response[0].nombre + ' ');
+    expect(elements[1].textContent).toEqual(' ' + response[1].nombre + ' ');
+    expect(elements[2].textContent).toEqual(' ' + response[2].nombre + ' ');
   });
 });
