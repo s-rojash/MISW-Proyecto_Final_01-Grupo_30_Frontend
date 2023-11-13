2a9932fdf50288a47e98de7cda843affb13b98f5
Oscar Alvarez <o.alvareze@uniandes.edu.co>
2023-11-12 22:18:32 -0500

    e agrega el logout en web para candidatos y empresas

---
+++ src/app/login/logout-page/logout-page.component.spec.ts
@@ -0,0 +1,60 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule} from '@angular/material/dialog';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogoutPageComponent } from './logout-page.component';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
+describe('LogoutPageComponent', () => {
+  let component: LogoutPageComponent;
+  let fixture: ComponentFixture<LogoutPageComponent>;
+  let toastrService: ToastrService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports:[MatDialogModule, HttpClientModule, MatCardModule, MatFormFieldModule, MatButtonModule, MatIconModule,
+        MatInputModule, ReactiveFormsModule, BrowserAnimationsModule,
+        TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+        }),
+        ToastrModule.forRoot({
+          timeOut: 10000,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true,
+      }),],
+      declarations: [ LogoutPageComponent ],
+      providers: [ ToastrService ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoutPageComponent);
+    component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+});
